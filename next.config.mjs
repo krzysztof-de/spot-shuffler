674b3194f267841b9abb1c974797e1e66dc97394
deptula_krzysztof <krzysztof.deptula@apagroup.pl>
2024-10-01 11:03:54 +0200

    api and next url

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:3000/",
+    API_URL: "https://neighborhood-drift.vercel.app",
     DB_LOCAL_URI: "mongodb://localhost:27017/places",
     DB_URI: "mongodb://localhost:27017/places",
 
-    NEXTAUTH_URL: "http://localhost:3000/",
+    NEXTAUTH_URL: "https://neighborhood-drift.vercel.app",
     NEXTAUTH_SECRET: "DOASKPODKAPODALMAMSDAMDASDSADASDQWedqad8",
   },
   images: {
