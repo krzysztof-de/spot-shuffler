22f6d1e735149bd511b7a3183e518064784ea0ad
deptula_krzysztof <krzysztof.deptula@apagroup.pl>
2024-10-03 09:32:09 +0200

    uris setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://neighborhood-drift.vercel.app/",
+    API_URL: process.env.NODE_ENV === 'production'
+      ? "https://neighborhood-drift.vercel.app/"
+      : process.env.NODE_ENV === 'preview'
+      ? "https://neighborhood-drift.vercel.app/"
+      : "http://localhost:3000/",
     DB_LOCAL_URI: "mongodb://localhost:27017/places",
-
-    NEXTAUTH_URL: "https://neighborhood-drift.vercel.app/",
+    NEXTAUTH_URL: process.env.NODE_ENV === 'production'
+      ? "https://neighborhood-drift.vercel.app/"
+      : process.env.NODE_ENV === 'preview'
+      ? "https://neighborhood-drift.vercel.app/"
+      : "http://localhost:3000/",
     NEXTAUTH_SECRET: "DOASKPODKAPODALMAMSDAMDASDSADASDQWedqad8",
   },
   images: {
